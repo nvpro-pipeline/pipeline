cf2d6f32836de5b761acfbcde89cc84e1dcb879c
Andreas Süßenbach <asuessenbach@nvidia.com>
2015-02-23 15:25:07 +0100

    Adjusted workspace to work with private DAELoader, EXRLoader, FLTLoader, MBSDFLoader, OBJLoader; removed some warnings with CMake
    Change-Id: I60bb5daa500bb372b9902c4d47273f7c2385d428
    Reviewed-on: http://git-master/r/710396
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Ingo Esser <iesser@nvidia.com>
    Tested-by: Ingo Esser <iesser@nvidia.com>

--- CMake/FindNVfltLib.cmake
+++ CMake/FindNVfltLib.cmake
@@ -5,16 +5,20 @@
 #  FLTLIB_LIBRARIES - The libraries needed to use fltlib
 #  FLTLIB_DEFINITIONS - Compiler switches required for using fltlib
 
-find_path(FLTLIB_INCLUDE_DIR "flt.h")
-find_library(FLTLIB_LIBRARY NAMES OpenFlight.lib)
+file( TO_CMAKE_PATH "$ENV{DP_3RDPARTY_PATH}/fltLib/" FLTLIBROOT )
 
-set(FLTLIB_LIBRARIES ${FLTLIB_LIBRARY} )
-set(FLTLIB_INCLUDE_DIRS ${FLTLIB_INCLUDE_DIR} )
+if ( EXISTS "${FLTLIBROOT}" )
+  set( FLTLIB_FOUND        "true" )
+  set( FLTLIB_INCLUDE_DIRS "${FLTLIBROOT}" )
+  set( FLTLIB_LIBRARIES    optimized "${FLTLIBROOT}/win32-msvc${MSVC_VERSION}-${DP_ARCH}/lib/Release/OpenFlight.lib"
+                           debug     "${FLTLIBROOT}/win32-msvc${MSVC_VERSION}-${DP_ARCH}/lib/Debug/OpenFlight.lib" )
+endif()
+
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set FLTLIB_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(fltlib  DEFAULT_MSG
-                                  FLTLIB_LIBRARY FLTLIB_INCLUDE_DIR)
+                                  FLTLIB_LIBRARIES FLTLIB_INCLUDE_DIRS)
 
-mark_as_advanced(FLTLIB_INCLUDE_DIR FLTLIB_LIBRARY )
+mark_as_advanced(FLTLIB_INCLUDE_DIRS FLTLIB_LIBRARIES )
