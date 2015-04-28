2de030c6666217ad1e084501d6a508fc68834646
mcleary <tluisrs@gmail.com>
2015-04-28 11:48:53 -0300

    Little fix in .gitignore. It was written ftlLib but since git is case sensitive changed it to ftllib
    Code to identify the most recent Visual Studio version installed in the machine
    Also sets the command line tools to build the 3rdparty dependencies.
    
    Include code in the 3rdPartyBuild.cmake header to allow the user to choose with dependencies to build.
    
    Added a check for MSVC13
    
    Add code to set DPHOME and DP_3RDPARTY_PATH in the script 3rdPartyBuild.cmd
    
    . RenderContextFormat.h need to include <algorithm> for std::max function
    
    CMakeLists.txt for several projects are now ignoring the #pragma comment(lib, "freeglutd.lib") preset in freeglut.h. This causes a compilation error since the freeglut being used as 3rdparty dependency only comes in release version
    
    The #error macro in HandledObject for explicit operator bool() was commented because it is working fine.
    
    3rdPartyBuild.cmd creates the builds subfolder
    
    Update in the README.txt
    
    Remove the need to set DPHOME and DP_3RDPARTY_PATH as environment variables. The main CMakeLists.txt do this in the beginning.
    
    Fix in CopyQtDlls tool. No need to set environment variable QTDIR. Viewer/CMakeLists.txt finds the Qt home from Qt5Core_DIR path.
    
    Remove instructions to set environment variables
    
    Remove instructions from README.txt to set environment variables.
    
    Build freeglut from the source code
    
    Suggestions to pr/2
    
    Suggestions make to the pull request #2 into nvpro-pipeline/pipeline
    
    msvc2010 compiler support for building boost

--- CMake/dp3rdParty.cmake
+++ CMake/dp3rdParty.cmake
@@ -1,7 +1,5 @@
 include("dp")
 
-file( TO_CMAKE_PATH "$ENV{DP_3RDPARTY_PATH}" DP_3RDPARTY_PATH)
-
 if( UNIX )
   set(Boost_USE_STATIC_LIBS       OFF)
   set(Boost_USE_MULTITHREADED      ON)
@@ -13,7 +11,7 @@ if(WIN32)
   ### boost
   if (NOT BOOSTROOT)
     file( TO_CMAKE_PATH "${DP_3RDPARTY_PATH}/Boost" BOOSTROOT)
-    
+
     if ( EXISTS "${BOOSTROOT}" )
       set( BOOST_ROOT "${BOOSTROOT}")
       set(Boost_USE_STATIC_LIBS "ON")
@@ -24,7 +22,7 @@ if(WIN32)
   # Currently disabled since a new version is required
   #list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/fltlib/include")
   #list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/fltlib/lib")
-  
+
   ### lib3ds
   list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/lib3ds/include")
   list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/lib3ds/lib")
@@ -36,7 +34,7 @@ if(WIN32)
   ### OpenEXR
   list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/OpenEXR/include")
   list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/OpenEXR")
-  
+
   ### DevIL
   list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/DevIL/include")
   list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/DevIL")
@@ -45,14 +43,14 @@ if(WIN32)
   list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/glew/include")
   # TODO OS arch dependency?
   list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/glew/lib/Release/x64")
-  
+
   ### freeglut
   list(APPEND CMAKE_INCLUDE_PATH "${DP_3RDPARTY_PATH}/freeglut/include")
   # TODO OS arch dependency?
-  list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/freeglut/lib/x64")
+  list(APPEND CMAKE_LIBRARY_PATH "${DP_3RDPARTY_PATH}/freeglut/lib")
 
   ### Qt
-  
+
   # Search for Windows SDK so that Qt5 can find glu32.lib
   if(WIN32)
     foreach(WINKIT "KitsRoot81" "KitsRoot")
@@ -65,27 +63,27 @@ if(WIN32)
         else()
           message(FATAL "unsupported architecture")
         endif()
-        
+
         find_path(WINKIT_LIB GlU32.lib PATHS "${WINKIT_DIR}/Lib/win8/um/${WINKIT_ARCH}" "${WINKIT_DIR}/Lib/winv6.3/um/${WINKIT_ARCH}")
-        
+
         list(APPEND CMAKE_PREFIX_PATH "${WINKIT_LIB}")
         message("Using Windows Kit ${WINKIT_DIR}")
         set(WINKIT_FOUND TRUE)
         break()
       endif()
     endforeach()
-    
+
     if (NOT ${WINKIT_FOUND})
       message("Windows Kit not found. Qt might not find glu32.lib")
     endif()
   endif()
-    
+
   get_filename_component(QtRoot "[HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Qt;InstallLocation]" ABSOLUTE)
   if (EXISTS ${QtRoot})
     if (${DP_ARCH} STREQUAL "amd64")
       set(QtArch "_64")
     endif()
-    
+
     if (MSVC11)
       set(QtCompiler "msvc2012")
       if (${DP_ARCH} STREQUAL "amd64")
@@ -93,15 +91,20 @@ if(WIN32)
       else()
         set(QtVersion "5.3")
       endif()
-    else()
+    elseif(MSVC12)
       set(QtCompiler "msvc2013")
-      set(QtVersion "5.3")
+      set(QtVersion "5.4")
+
+	  if(NOT EXISTS "${QtRoot}/${QtVersion}")
+		message("Qt 5.4 not found. Fall back to Qt 5.3")
+		set(QtVersion "5.3")
+	  endif()
     endif()
-    
+
     list(APPEND CMAKE_PREFIX_PATH "${QtRoot}/${QtVersion}/${QtCompiler}${QtArch}_opengl/lib/cmake")
-    
+
   endif()
- 
+
 endif()
 
 macro(CopyDevIL)
